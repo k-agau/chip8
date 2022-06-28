4437db03628ac744d95a83638add93794ca008e0
k-agau <ksilvergold18@gmail.com>
2022-06-28 16:31:33 -0400

    Updates to opcode

--- include/CPU/chip8.h
+++ include/CPU/chip8.h
@@ -1,12 +1,13 @@
 #ifndef CHIP8_H
 #define CHIP8_H
 
+#include <cmath>
 #include <cstdint>
 #include <cstdio>
 #include <cstring>
 #include <string>
 
-const unsigned char chip8_fontset[80] =
+const unsigned char CHIP8_FONT_SET[80] =
 {
         0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
         0x20, 0x60, 0x20, 0x20, 0x70, // 1
@@ -25,6 +26,21 @@ const unsigned char chip8_fontset[80] =
         0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
         0xF0, 0x80, 0xF0, 0x80, 0x80  // F
 };
+
+const unsigned char ORIGINAL_KEY_MAP[16] = {
+    '1','2','3','C',
+    '4','5','6','D',
+    '7','8','9','E',
+    'A','0','B','F'
+};
+
+const unsigned char NEW_KEY_MAP[16] = {
+    '1','2','3','4',
+    'Q','W','E','R',
+    'A','S','D','F',
+    'Z','X','C','V'
+};
+
 const uint32_t BUFFER_SIZE = 512;
 
 class chip8 {
@@ -63,72 +79,80 @@ private:
     std::FILE* loadGame(const char* name);
     inline unsigned short xReg();
     inline unsigned short yReg();
-    // Functions
+    // opcode
+    
     // 0NNN
     void op0();
-
     // 00E0
     void op1();
-
     // 00EE
     void op2();
-
     // 1NNN
     void op3();
-
     // 2NNN
     void op4();
-
     // 3XNN
     void op5();
-
     // 4XNN
     void op6();
-
     // 5XY0
     void op7();
-
     // 6XNN
     void op8();
-
     // 7XNN
     void op9();
-
     // 8XY0
     void op10();
-
     // 8XY1
     void op11();
-
     // 8XY2
     void op12();
-
     // 8XY3
     void op13();
     // 8XY4
     void op14();
     // 8XY5
+    void op15();
     // 8XY7
+    void op16();
     // 8XY7
+    void op17();
     // 8XYE
+    void op18();
     // 9XY0
+    void op19();
     // ANNN
+    void op20();
     // BNNN
+    void op21();
     // CXNN
+    void op22();
     // DXYN
     void op23();
+
+
     // EX9E
+    void op24();
     // EXA1
+    void op25();
     // FX07
+    void op26();
     // FX0A
+    void op27();
     // FX15
+    void op28();
     // FX18
+    void op29();
     // FX1E
+    void op30();
     // FX29
+    void op31();
     // FX33
     void op32();
     // FX55
+    void op33();
     // FX65
+    void op34();
 
 public:
     chip8();
@@ -137,4 +161,4 @@ public:
     bool getDrawFlag();
 };
 
-#endif // chip8
+#endif // CHIP8_H
--- src/CPU/chip8.cpp
+++ src/CPU/chip8.cpp
@@ -49,13 +49,18 @@ void chip8::emulateCycle() {
             op32();
             break;
     }
-
-    delay_timer++;
-    sound_timer++;
-    key[0]++;
     pc += 2;
 
-
+    if(delay_timer > 0) {
+        --delay_timer;
+    }
+    if(sound_timer > 0) {
+        if(sound_timer == 1) {
+            printf("BEEP!\n");
+            --sound_timer;
+        }
+    }
+    key[0]++;
 }
 
 void chip8::clearDisplay() {
@@ -76,7 +81,7 @@ void chip8::clearMemory() {
 
 void chip8::loadFont() {
     for(uint32_t i = 0; i < 80; ++i) {
-        memory[i] = chip8_fontset[i];
+        memory[i] = CHIP8_FONT_SET[i];
     }
 }
 
@@ -100,6 +105,19 @@ inline unsigned short chip8::xReg() {
 
 inline unsigned short chip8::yReg() {
     return (opcode & 0x00F0) >> 4;
+}
+
+void chip8::op0() {
+    pc = (opcode & 0x0FFF);
+}
+
+void chip8::op1() {
+    clearDisplay();
+}
+
+void chip8::op2() {
+    pc = stack[--sp];
+
 }
 
 void chip8::op3() {
@@ -138,6 +156,22 @@ void chip8::op9() {
     V[xReg()] += (opcode & 0x00FF);
 }
 
+void chip8::op10() {
+    V[xReg()] = V[yReg()];
+}
+
+void chip8::op11() {
+    V[xReg()] |= V[yReg()];
+}
+
+void chip8::op12() {
+    V[xReg()] &= V[yReg()];
+}
+
+void chip8::op13() {
+    V[xReg()] ^= V[yReg()];
+}
+
 void chip8::op14() {
     if(V[yReg()] > (0xFF - V[xReg()])) {
         V[0xF] = 1; // Carry
@@ -147,8 +181,116 @@ void chip8::op14() {
     V[xReg()] += V[yReg()];
 }
 
+void chip8::op15() {
+    if(V[yReg()] > (0xFF - V[xReg()])) {
+        V[0xF] = 0;
+    } else {
+        V[0xF] = 1;
+    }
+    V[xReg()] -= V[yReg()];
+}
+
+void chip8::op16() {
+    V[0xF] = V[xReg()] & 1;
+    V[xReg()] >>= 1;
+}
+
+void chip8::op17() {
+    if(V[xReg()] > (0xFF - V[yReg()])) {
+        V[0xF] = 0;
+    } else {
+        V[0xF] = 1;
+    }
+    V[xReg()] = V[yReg()] - V[xReg()];
+}
+
+void chip8::op18() {
+    V[0xF] = 1 << (unsigned char)(std::log2((V[xReg()])));
+    V[xReg()] <<= 1;
+}
+
+void chip8::op19() {
+    if(V[xReg()] != V[yReg()]) {
+        pc += 2;
+    }
+}
+
+void chip8::op20() {
+    I = (opcode & 0x0FFF);
+}
+
+void chip8::op21() {
+    pc = V[0x0] + (opcode & 0x0FFF);
+}
+
+void chip8::op22() {
+    V[xReg()] = std::rand() & (opcode & 0x00FF);
+}
+
+void chip8::op23() {
+    unsigned short xCord = V[xReg()], yCord = V[yReg()], height = (opcode & 0x000F);
+    unsigned short pixel;
+
+    for(unsigned short yLine = 0; yLine < height; ++ yLine) {
+        pixel = memory[I + yLine];
+        for(unsigned short xLine = 0; xLine < 8; ++ xLine) {
+            if((pixel & (0x80 >> xLine)) != 0) {
+                if(gfx[(xCord + xLine + ((yLine + yLine) * 64))] == 1) {
+                    V[0xF] = 1;
+                }
+                gfx[xCord + xLine + ((yCord + yLine) * 64)] ^= 1;
+            }
+        }
+    }
+    drawFlag = true;
+}
+
+void chip8::op24() {
+
+}
+
+void chip8::op25() {
+
+}
+
+void chip8::op26() {
+    V[xReg()] = delay_timer;
+}
+
+void chip8::op27() {
+
+}
+
+void chip8::op28() {
+    delay_timer = V[xReg()];
+}
+
+void chip8::op29() {
+    sound_timer = V[xReg()];
+}
+
+void chip8::op30() {
+    I += V[xReg()];
+}
+
+void chip8::op31() {
+    I = CHIP8_FONT_SET[V[xReg()]];
+}
+
 void chip8::op32() {
-    memory[I] =     V[xReg()] / 100;
+    memory[I]     = V[xReg()] / 100;
     memory[I + 1] = V[xReg()] / 10 % 10;
     memory[I + 2] = V[xReg()] % 100 % 10;
+}
+
+void chip8::op33() {
+    for(unsigned short i = 0; i <= xReg(); ++i) {
+        memory[I + i] = V[i];
+    }
+}
+
+void chip8::op34() {
+    for(unsigned short i = 0; i <= xReg(); ++i) {
+        V[i] = memory[I + i];
+    }
 }
